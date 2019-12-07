889ff2a7caf5900368f85bb665c1e5f052e87d9e
pulkit <nbt@nbts-MacBook-Pro-2.local>
2019-12-07 21:11:07 +0530

    added alert and local storage functionality

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
-
+function storeBooks(){}
+storeBooks.getBooks = function getBooks(){
+    const books = JSON.parse(localStorage.getItem('books'));
+    if (books === null){
+        return [];
+    }
+    return books;
+}
+storeBooks.addBook = function addBook(book){
+    const books = storeBooks.getBooks();
+    books.push(book);
+    localStorage.setItem('books',JSON.stringify(books));
+}
+storeBooks.removeBook = function removeBook(isbn){
+    const books = storeBooks.getBooks();
+    books.forEach((book, index)=> {
+        if (book.isbn === isbn){
+            books.splice(index,1);
+        }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+}
 function book(title, author, isbn){
     this.title = title;
     this.author = author;
     this.isbn = isbn
 }
 function displayBooks(){
-    const books = [
-        {
-            title: "Pulkit",
-            author: "Poki",
-            isbn: 123
-        },
-        {
-            title: "simple",
-            author: "dn",
-            isbn: 1234
-        },
-        {
-            title: "jdajkf",
-            author: "df",
-            isbn: 12
-        }
-    ]
+    const books = storeBooks.getBooks();
     books.forEach((book)=> addBook(book))
 }
+
 function addBook(book){
     const list = document.querySelector("#book-list");
     const row = document.createElement('tr');
@@ -38,7 +44,19 @@ function addBook(book){
 function deleteRow(row){
     if (row.classList.contains('delete')){
         row.parentElement.parentElement.remove();
+        storeBooks.removeBook();
     }
+}
+function showAlerts(message, type){
+    const div = document.createElement('div');
+    div.className = `alert alert-${type}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.form-container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+    setTimeout(()=> {
+        div.remove()
+    }, 4000)
 }
 function clearFields(){
     document.querySelector("#title").value = "";
@@ -55,13 +73,21 @@ document.addEventListener('DOMContentLoaded', ()=> {
         const title = document.querySelector("#title").value;
         const author = document.querySelector("#author").value;
         const isbn = document.querySelector("#isbn").value;
-        const newBook = new book(title, author, isbn);
-        addBook(newBook);
-        clearFields();
+        if (title === '' || author === '' || isbn === ''){
+            showAlerts('Please fill all the details', 'danger');
+        }
+        else{
+            const newBook = new book(title, author, isbn);
+            addBook(newBook);
+            storeBooks.addBook(newBook);
+            clearFields();
+            showAlerts('Book added successfully', 'success');
+        }
     })
     const list = document.querySelector("#book-list");
     list.addEventListener("click", (e)=> {
         deleteRow(e.target)
+        storeBooks.removeBook(e.target.parentElement.previousElementSibling.innerText)
     })
     
 })
